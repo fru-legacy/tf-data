bddb6a3b4f4466d26553aef8e9086ffa59db2f3d
Florian Rüberg <florian.rueberg@gmail.com>
2018-03-17 18:45:55 +0100

    Add placeholder builder

--- tf-data/__init__.py
+++ tf-data/__init__.py
@@ -1,16 +1,18 @@
 import drive
 import os
-import numpy as np
+from LabeledImageDataset import LabeledImageDataset
 
 
 def mnist(path, file='mnist.npz'):
     drive.download(path, file, '18JJ5hzZC_pKF7dfpHHn2Ca2-7JZrHSqJ', 11507640)
-    return np.load(os.path.join(path, file))
+    return LabeledImageDataset(28, 28, 1, 10, os.path.join(path, file), lambda x: x / 255.0)
 
 
 def fashion_mnist(path, file='fashion-mnist.npz'):
     drive.download(path, file, '1RptZ_WVaGvRTtn1Kf2UHWn0QkB5DIhJa', 30888555)
-    return np.load(os.path.join(path, file))
+    return LabeledImageDataset(28, 28, 1, 10, os.path.join(path, file), lambda x: x / 255.0)
 
-#print(len(fashion_mnist('/data/tf-data')['train_labels']))
-#print(fashion_mnist('/data/tf-data')['train_labels'][0:5])
+# Usage example:
+# data = fashion_mnist('/data/tf-data').placeholder()
+# print(data.test(splits=2)[0][data.image_flat].shape)
+# print(data.train()[data.image_flat].shape)
